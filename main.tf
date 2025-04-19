8f24e734f4e2b9c603bb734ea6097177960d37c8
Igor Flores <dev.igorflore@gmail.com>
2025-04-19 14:10:09 -0300

    Finalizado curso

--- main.tf
+++ main.tf
@@ -10,15 +10,20 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-075686beab831bb7f"
   instance_type = "t2.micro"
   key_name      = "igorfloresdev"
-
+  # user_data = <<-EOF
+  #                 #!/bin/bash
+  #                 cd /home/ubuntu
+  #                 echo "<h1>Feito com Terraform</h1>" > index.html
+  #                 nohup busybox httpd -f -p 8080 &
+  #               EOF
   tags = {
-    Name = "primeira-instancia"
+    Name = "Terraform Ansible Python"
   }
 }
